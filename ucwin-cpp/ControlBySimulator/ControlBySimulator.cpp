e6cfccac007c1a5e9793ef6b16fd721e67e6eef8
MoonGyu-Jeong <newsm5403@kookmin.ac.kr>
2023-03-18 15:34:51 +0900

    feat: control current vehicle (#5)

--- ucwin-cpp/ControlBySimulator/ControlBySimulator.cpp
+++ ucwin-cpp/ControlBySimulator/ControlBySimulator.cpp
@@ -1,5 +1,4 @@
 #include "ControlBySimulator.h"
-#include <iostream>
 
 ControlBySimulator::ControlBySimulator()
 {
@@ -28,20 +27,29 @@ void ControlBySimulator::StartProgram()
     ribbonButton1 = ribbonGroup->CreateButton(L"ButtonControlBySimulator1");
     ribbonButton1->SetCaption(L"Get CAN data");
     ribbonButton1->SetWidth(120);
-    Cb_RibbonMenuItemOnClick callback1 = std::bind(&ControlBySimulator::OnButtonGetCANDataClick, this);
-    p_cbHandle1 = ribbonButton1->SetCallbackOnClick(callback1);
+    Cb_RibbonMenuItemOnClick callback1 = 
+        std::bind(&ControlBySimulator::OnButtonGetCANDataClick, this);
+    p_cbHandleButtonClick1 = ribbonButton1->SetCallbackOnClick(callback1);
 
     ribbonButton2 = ribbonGroup->CreateButton(L"ButtonControlBySimulator2");
     ribbonButton2->SetCaption(L"Control Vehicle");
     ribbonButton2->SetWidth(120);
-    Cb_RibbonMenuItemOnClick callback2 = std::bind(&ControlBySimulator::OnButtonControlCarClick, this);
-    p_cbHandle2 = ribbonButton2->SetCallbackOnClick(callback2);
+    Cb_RibbonMenuItemOnClick callback2 = 
+        std::bind(&ControlBySimulator::OnButtonControlCarClick, this);
+    p_cbHandleButtonClick2 = ribbonButton2->SetCallbackOnClick(callback2);
+
+    F8TrafficSimulationProxy traffic = 
+        g_applicationServices->GetSimulationCore()->GetTrafficSimulation();
+    Cb_TrafficTransientObjectDeleted callbackTransientDeleted = 
+        std::bind(&ControlBySimulator::OnTransientDeleted, this, std::placeholders::_1);
+    p_cbHandleTransientDeleted = 
+        traffic->RegisterEventTransientObjectDeleted(callbackTransientDeleted);
 }
 
 void ControlBySimulator::StopProgram()
 {
-    ribbonButton1->UnsetCallbackOnClick(p_cbHandle1);
-    ribbonButton2->UnsetCallbackOnClick(p_cbHandle2);
+    ribbonButton1->UnsetCallbackOnClick(p_cbHandleButtonClick1);
+    ribbonButton2->UnsetCallbackOnClick(p_cbHandleButtonClick2);
     ribbonGroup->DeleteControl(ribbonButton1);
     ribbonGroup->DeleteControl(ribbonButton2);
     ribbonTab->DeleteGroup(ribbonGroup);
@@ -49,32 +57,90 @@ void ControlBySimulator::StopProgram()
         g_applicationServices->GetMainForm()->GetMainRibbonMenu()->DeleteTab(ribbonTab);
 }
 
-void ControlBySimulator::OnButtonGetCANDataClick()
+void ControlBySimulator::OnTransientDeleted(F8TransientInstanceProxy instance)
 {
-    
+    std::wstring proxyName = instance->GetProxyName();
+    if (instance->GetTransientType() == _TransientCar)
+    {
+        F8TransientCarInstanceProxy carProxy = 
+            std::static_pointer_cast<F8TransientCarInstanceProxy_Class>(instance);
+        RemoveControlledInstance(carProxy);
+    }
 }
 
 void ControlBySimulator::OnButtonControlCarClick()
 {
+    F8MainDriverProxy driver = g_applicationServices->GetSimulationCore()->GetTrafficSimulation()->GetDriver();
+    if (Assigned(driver)) {
+        F8TransientCarInstanceProxy car = driver->GetCurrentCar();
+        if (Assigned(car))
+        {
+            RemoveControlledInstance(car);
+            AddControlledInstance(car);
+        }
+    }
+}
 
+void ControlBySimulator::RemoveControlledInstance(F8TransientCarInstanceProxy inst)
+{
+    decltype(vehicleDataDict)::iterator itr;
+    itr = vehicleDataDict.find(inst->GetID());
+    if (itr != vehicleDataDict.end())
+    {
+        inst->UnregisterCallbackOnBeforeCalculateMovement(itr->second.cbHandleOnBeforeCalculateMovement);
+        vehicleDataDict.erase(itr);
+    }
+}
+
+void ControlBySimulator::AddControlledInstance(F8TransientCarInstanceProxy inst)
+{
+    VehicleData data;
+    data.proxy = inst;
+    Cb_TransientOnBeforeCalculateMovement callback1 = 
+        std::bind(&ControlBySimulator::OnVehicleBeforeCalculateMovement, this, std::placeholders::_1, std::placeholders::_2);
+    data.cbHandleOnBeforeCalculateMovement = 
+        inst->RegisterCallbackOnBeforeCalculateMovement(callback1);
+    vehicleDataDict.insert(std::make_pair(inst->GetID(), data));
+}
+
+void ControlBySimulator::OnVehicleBeforeCalculateMovement(double dTime, F8TransientInstanceProxy instance)
+{
+    if (instance->GetTransientType() == _TransientCar) {
+        F8TransientCarInstanceProxy proxyCar = 
+            std::static_pointer_cast<F8TransientCarInstanceProxy_Class>(instance);
+        ControlVehicle(proxyCar, dTime);
+    }
+}
+
+void ControlBySimulator::ControlVehicle(F8TransientCarInstanceProxy& proxyCar, double time)
+{
+    decltype(vehicleDataDict)::iterator itr;
+    itr = vehicleDataDict.find(proxyCar->GetID());
+    if (itr != vehicleDataDict.end()) {
+        const auto& data = itr->second;
+        double steering = data.steering;
+        double throttle = data.throttle;
+        double brake = data.brake;
+
+        proxyCar->SetEngineOn(true);
+        proxyCar->SetSteering(steering);
+        proxyCar->SetThrottle(throttle);
+        proxyCar->SetBrake(brake);
+        proxyCar->SetClutch(0.0);
+    }
+}
+
+void ControlBySimulator::OnButtonGetCANDataClick()
+{
+    ReceiveCANData();
 }
 
 void ControlBySimulator::ReceiveCANData()
 {
-
+    DataParser();
 }
 
 void ControlBySimulator::DataParser()
 {
-
-}
-
-void ControlBySimulator::ControlVehicle()
-{
-
-}
-
-void ControlBySimulator::OnVehicleBeforeCalculateMovement()
-{
-
+    
 }
--- ucwin-cpp/ControlBySimulator/ControlBySimulator.h
+++ ucwin-cpp/ControlBySimulator/ControlBySimulator.h
@@ -2,6 +2,20 @@
 #include "F8API.h"
 #include "pch.h"
 #include "windows.h"
+#include <map>
+#include <iostream>
+#include <memory>
+
+struct VehicleData {
+	F8TransientInstanceProxy proxy;
+	void* cbHandleOnBeforeCalculateMovement;
+	double time = 0.0;
+	double steering = 0.0;
+	double throttle = 1.0;
+	double brake = 0.0;
+};
+
+static const double pi = 3.141592653589793;
 
 class ControlBySimulator
 {
@@ -9,9 +23,10 @@ private:
 	F8MainRibbonTabProxy ribbonTab;
 	F8MainRibbonGroupProxy ribbonGroup;
 	F8MainRibbonButtonProxy ribbonButton1, ribbonButton2;
-	void* p_cbHandle1, * p_cbHandle2;
-	
+	void* p_cbHandleButtonClick1, * p_cbHandleButtonClick2, * p_cbHandleTransientDeleted;
 
+	std::map<int, VehicleData> vehicleDataDict;
+	
 public:
 	ControlBySimulator();
 	~ControlBySimulator();
@@ -20,11 +35,14 @@ public:
 	void StopProgram();
 
 private:
-	void OnButtonGetCANDataClick();
 	void OnButtonControlCarClick();
+	void OnTransientDeleted(F8TransientInstanceProxy instance);
+	void RemoveControlledInstance(F8TransientCarInstanceProxy inst);
+	void AddControlledInstance(F8TransientCarInstanceProxy inst);
+	void OnVehicleBeforeCalculateMovement(double dTime, F8TransientInstanceProxy instance);
+	void ControlVehicle(F8TransientCarInstanceProxy& proxyCar, double time);
+
+	void OnButtonGetCANDataClick();
 	void ReceiveCANData();
 	void DataParser();
-	void ControlVehicle();
-	void OnVehicleBeforeCalculateMovement();
 };
-
